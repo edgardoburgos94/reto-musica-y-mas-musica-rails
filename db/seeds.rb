14a5542f55b55d2272b24b238075b827124915df
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-15 01:55:26 -0500

    Ver artistas y albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+juanes = Artist.create(name:"Juanes", image_url: 'http://estaticos.elmundo.es/social/static/img/avatars/xlarge_default.png')
+shakira = Artist.create(name:"Shakira", image_url: 'http://estaticos.elmundo.es/social/static/img/avatars/xlarge_default.png')
+morat = Artist.create(name:"Morat", image_url: 'http://estaticos.elmundo.es/social/static/img/avatars/xlarge_default.png')
+
+fijate_bien = Album.create(name:"Fijate bien",  image_url:'https://srv.latostadora.com/canvas3D.dll/disco--i:13562356341701356232116321;h:270;s:D_G1;b:F1F1F1;k:b172c13c5cbe60dda33666167cb8aa15.jpg' )
+un_dia_normal = Album.create(name:"Un día normal",  image_url:'https://srv.latostadora.com/canvas3D.dll/disco--i:13562356341701356232116321;h:270;s:D_G1;b:F1F1F1;k:b172c13c5cbe60dda33666167cb8aa15.jpg' )
+mi_sangre = Album.create(name:"Mi sangre",  image_url:'https://srv.latostadora.com/canvas3D.dll/disco--i:13562356341701356232116321;h:270;s:D_G1;b:F1F1F1;k:b172c13c5cbe60dda33666167cb8aa15.jpg' )
+la_vida_es_un_ratico = Album.create(name:"La vida es un ratico",  image_url:'https://srv.latostadora.com/canvas3D.dll/disco--i:13562356341701356232116321;h:270;s:D_G1;b:F1F1F1;k:b172c13c5cbe60dda33666167cb8aa15.jpg' )
+
+juanes.albums << fijate_bien
+juanes.albums << un_dia_normal
+juanes.albums << mi_sangre
+juanes.albums << la_vida_es_un_ratico
